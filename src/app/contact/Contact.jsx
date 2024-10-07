58c52b5020505ca3472f40ec4e0875535656f420
jai <jai@pearlorganisation.com>
2024-10-07 17:25:59 +0530

    contact form

--- src/app/contact/Contact.jsx
+++ src/app/contact/Contact.jsx
@@ -101,7 +101,7 @@ const Contact = () => {
                   </div>
                   <div>
                     <input
-                      type="text"
+                      type="email"
                       {...register("email", { required: "Email is required" })}
                       id="email"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
